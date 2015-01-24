7a0c2ecdc53e2a191fc2de73819540d89b5be1e3
loadrunner <load.runner@weednet.ro>
2015-01-24 15:28:12 +0200

    spacing

--- Classes/AppDelegate.h
+++ Classes/AppDelegate.h
@@ -32,9 +32,9 @@ public:
 	virtual bool applicationDidFinishLaunching();
 	
 	/**
-	@brief  The function be called when the application enter background
-	@param  the pointer of the application
-	*/
+	 @brief  The function be called when the application enter background
+	 @param  the pointer of the application
+	 */
 	virtual void applicationDidEnterBackground();
 	
 	/**
